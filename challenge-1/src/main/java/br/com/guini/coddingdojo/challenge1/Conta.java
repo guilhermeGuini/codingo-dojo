630144ab2f00d787464bd1d534933e5e011f95ff
Guilherme Silva Guini <guilhermeguini@gmail.com>
2023-01-18 14:51:03 -0300

    Example commit

--- challenge-1/src/main/java/br/com/guini/coddingdojo/challenge1/Conta.java
+++ challenge-1/src/main/java/br/com/guini/coddingdojo/challenge1/Conta.java
@@ -27,4 +27,21 @@ public class Conta {
     public BigDecimal debita(BigDecimal valor) {
         throw new UnsupportedOperationException();
     }
+
+    public BigDecimal getLimite() {
+        // saldo positivo, não usou limite
+        if (saldo.compareTo(BigDecimal.ZERO) >= 0) {
+            return limite;
+        }
+
+        if (saldo.abs().compareTo(limite) < 0) {
+            return limite.min(saldo.abs());
+        }
+
+        return BigDecimal.ZERO;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
 }
--- challenge-1/src/test/java/br/com/guini/coddingdojo/challenge1/ContaTest.java
+++ challenge-1/src/test/java/br/com/guini/coddingdojo/challenge1/ContaTest.java
@@ -1,4 +1,25 @@
 package br.com.guini.coddingdojo.challenge1;
 
-public class ContaTest {
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ContaTest {
+
+    private Conta contaSemLimite = new Conta();
+    private Conta contaComLimite = new Conta(BigDecimal.valueOf(1000), BigDecimal.valueOf(25000));
+
+    @Test
+    void nao_deve_permitir_sacar_valor_maior_que_saldo() {
+
+    }
+
+    @Test
+    void testLimit() {
+        assertNull(contaSemLimite.getLimite());
+        assertTrue(contaComLimite.getLimite().compareTo(BigDecimal.valueOf(1000)) == 0);
+    }
 }
