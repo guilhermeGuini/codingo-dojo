434db8ce3806b7c4d28625afca368de45b9b4262
Guilherme Silva Guini <guilhermeguini@gmail.com>
2023-01-23 17:05:50 -0300

    Adicionando classe de teste

---
+++ challenge-1/src/test/java/br/com/guini/coddingdojo/challenge1/EstabelecimentoComercialTest.java
@@ -0,0 +1,18 @@
+package br.com.guini.coddingdojo.challenge1;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+class EstabelecimentoComercialTest {
+
+    /**
+     * EC = Estabelecimento Comercial
+     * Merchant = Estabelecimento
+     **/
+    private EstabelecimentoComercial ec = new EstabelecimentoComercial(BigDecimal.valueOf(1500), BigDecimal.ZERO);
+
+    @Test
+    void test() {
+    }
+}
